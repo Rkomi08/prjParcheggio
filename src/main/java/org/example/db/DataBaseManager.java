c2a0423c6f90348a14323d7b0ce7934ce82c62bc
Rkomi Ca <mirko.casto08@gmail.com>
2026-07-02 23:12:44 +0200

    Usa variabili d'ambiente per credenziali database

--- src/main/java/org/example/db/DataBaseManager.java
+++ src/main/java/org/example/db/DataBaseManager.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 
 public class DataBaseManager {
 
-    private static final String URL = "jdbc:mysql://sql7.freesqldatabase.com/sql7830758";
-    private static final String USER = "sql7830758";
-    private static final String PASSWORD = "lRzfaXeVMN";
-
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
